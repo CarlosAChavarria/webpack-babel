b9f71c263d32467927f87b8b5ec835e87f90f8c4
Carlos Alberto Chavarria Ortiz <carlosalberto.chavarriao@gmail.com>
2021-10-05 22:05:06 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import 'fsLightbox';
+fsLightboxInstances['first-lightbox'].open(0);
+fsLightboxInstances['second-lightbox'].props.onOpen = () => console.log('Lightbox open!');
+
+/*import { suma,saludo } from "./operaciones";
+import Swal from 'sweetalert2'
+console.log(suma(10,10));
+console.log(saludo);
+document.write(`<h1>${suma(5,20)}</h1>`);
+
+Swal.fire({
+    title: 'Sweet!',
+    text: 'Hola mundo! te saluda Carlos',
+    imageUrl: 'https://unsplash.it/400/200',
+    imageWidth: 150,
+    imageHeight: 150,
+    imageAlt: 'Custom image',
+  })
+ import _ from 'lodash';
+
+function component() {
+    const element = document.createElement('div');
+  
+    // Lodash, now imported by this script
+    // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  
+    return element;
+  }
+  
+  document.body.appendChild(component());*/
---
+++ src/main.js
@@ -0,0 +1,3 @@
+import { suma,saludo } from "./operaciones";
+console.log(suma(2,5));
+console.log(saludo);
